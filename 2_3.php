5fb6a9d05a8c0478ffa033086100d6c0d9132655
Gauresh <gaureshp2@gmail.com>
2019-05-02 10:46:17 +0530

    Leaning about OOPS in PHP FOR MVC

---
+++ 2_3.php
@@ -0,0 +1,12 @@
+<?php
+    // Define a class
+    class User {
+        // properties (attribute)
+            public $name = 'Gauresh';
+
+        // Methods (functions)
+    }
+
+    // Instantiate a user object from the user class
+    $user = new User();
+    echo $user->name;
